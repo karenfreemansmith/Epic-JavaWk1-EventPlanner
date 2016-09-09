43a678f660331dc8109b6871b265a4980c4e2a0a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-09 08:15:45 -0700

    add initialize code to test file, status passing

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,3 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class EventTest {
-  
+  private Event mEvent;
+
+  @Before
+  private void initialize() {
+    mEvent = new Event();
+  }
+
 }
